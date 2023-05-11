b89fb02cbc7d13c651b9acabd4bf6e2a5a3c3e15
Darnell Baird <drsbaird@yahoo.com>
2023-05-11 15:08:35 -0400

    feat: Working autofill but not vetted

--- cli.py
+++ cli.py
@@ -1,19 +1,35 @@
+import time
+
 import click
 
 import lib
 
 @click.group()
+# Add verbose
 def cli():
     pass
 
+@cli.command()
+def hello_world():
+    click.echo("It works.")
+
+@cli.command()
 def type_header_on_screen():
     lib.type_header()
 
+@cli.command()
 def start_keyboard_listener():
+    click.echo("Keyboard listener initiated.")
     lib.start_keyboard_listener()
+    while lib.keyboard_listener is not None:
+        time.sleep(1)
+        click.echo("Broke-out the loop.")
+    time.sleep(1e4)
+    click.echo("Keyboard listener terminated.")
 
 cli.add_command(type_header_on_screen)
-
+cli.add_command(start_keyboard_listener)
+cli.add_command(hello_world)
 
 if __name__ == '__main__':
     cli()
--- lib.py
+++ lib.py
@@ -1,21 +1,9 @@
 import os
 
-from pynput.keyboard import Key, Controller
-from pynput import keyboard
+import pynput
+from pynput.keyboard import Key
 
 import config
-
-keyboard_typer = Controller()
-
-keyboard_callback_functions = dict(
-    (Key.shift, keyboard.KeyCode('l')):lambda: print("We made it."),
-)
-
-def keyboard_mapping_to_func(keys:tuple)->None:
-    """ 
-    Call a function according to the keys pressed / held down.
-    Comparison map is 'keyboard_callback_functions'
-    """
 
 
 def type_header(sep="\t"):
@@ -29,35 +17,83 @@ def read_text_file(file_path)->str:
         text = "".join(file.readlines())
     return text
 
-def type_type_template_file(type_template_path)->None:
+def type_type_template_file(type_template_path=config.TYPE_TEMPLATE_PATH)->None:
     """ Read type_template.txt file, type-out each character in the file. """
     text = read_text_file(type_template_path)
     keyboard_typer.type(text)
 
+# KEYBOARD CONTROLLER STUFF
+keyboard_typer = pynput.keyboard.Controller()
+keyboard_listener = None
+keyboard_keys_pressed = set()
 
-def on_press_keyboard_callback(key):
+keyboard_callback_functions = {
+    # (Key.shift, pynput.keyboard.KeyCode(char='l'), ): lambda: print("We made it."),
+    (pynput.keyboard.KeyCode(char='j'), ): lambda: print("A 'j' was pressed."),
+    # (pynput.keyboard.KeyCode(char='~'), ): lambda: print("I pressed it."),
+    # (Key.shift, Key.ctrl_l, pynput.keyboard.KeyCode(char='~'), ): type_type_template_file,
+    (Key.f9, ): type_type_template_file,
+}
+
+def call_func_mapped_to_key()->None:
+    """ 
+    Call a function according to the keys pressed / held down in 'keyboard_keys_pressed'.
+    Comparison map is 'keyboard_callback_functions'
+    """
+    callback_func = lambda: None
+    global keyboard_keys_pressed
+    # TODO: The following code structure needs to be unittest to confirm proper behavior of keys selected to function
+    for key_tup in keyboard_callback_functions.keys():
+        if all(k in key_tup for k in keyboard_keys_pressed):
+        # if all(k in keyboard_keys_pressed for k in key_tup):
+            callback_func = keyboard_callback_functions[key_tup]
+            break
+    # Now, call the function
+    callback_func()
+    return
+
+def on_press_keyboard_callback(key, verbose=0):
     """ Callback for 'start_keyboard_listener' function """
-    try:
-        print('alphanumeric key {0} pressed'.format(
-            key.char))
-    except AttributeError:
-        print('special key {0} pressed'.format(
+    global keyboard_keys_pressed
+    # Logging
+    if verbose > 0:
+        try:
+            print('alphanumeric key {0} pressed'.format(
+                key.char))
+        except AttributeError:
+            print('special key {0} pressed'.format(
+                key))
+
+def on_release_keyboard_callback(key, verbose=0):
+    """ Callback for 'start_keyboard_listener' function """
+    # Logging
+    if verbose > 0:
+        print('{0} released'.format(
             key))
-
-def on_release_keyboard_callback(key):
-    """ Callback for 'start_keyboard_listener' function """
-    print('{0} released'.format(
-        key))
-    if key == keyboard.Key.esc:
+    if key == pynput.keyboard.Key.esc:
         # Stop listener
         return False
+    # Finally, Remove the character from the set
+    global keyboard_keys_pressed
+    if key not in keyboard_keys_pressed:
+        # Once a new button is pressed, do the check
+        keyboard_keys_pressed.add(key)
+        print(keyboard_keys_pressed, " Keys held down.")
+        # Call the key callback function now. 
+        call_func_mapped_to_key()
+    if key in keyboard_keys_pressed:
+        keyboard_keys_pressed.remove(key)
 
 
 def start_keyboard_listener():
     """ Listen to keyboard inputs and execute appropriate commands. """
-    listener = keyboard.Listener(
+    keyboard_listener = pynput.keyboard.Listener(
         on_press=on_press_keyboard_callback,
         on_release=on_release_keyboard_callback)
-    listener.start()
+    keyboard_listener.start()
 
-def en
+def end_keyboard_listener():
+    """ Stop the keyboard listener. """
+    # pynput.keyboard.Listener.stop
+    if keyboard_listener is not None:
+        keyboard_listener.stop()
