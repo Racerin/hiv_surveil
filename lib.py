1d144233105cd5c1793b22ddc5da9d59280c7abc
Darnell Baird <drsbaird@yahoo.com>
2023-05-18 14:48:42 -0400

    Update: Not functioning code but must commit before leaving DMHC
    Start at line 61 in library.

--- lib.py
+++ lib.py
@@ -1,4 +1,5 @@
 import os
+from dataclasses import dataclass, Field
 
 import pynput
 from pynput.keyboard import Key
@@ -7,11 +8,18 @@ import config
 
 
 # KEYBOARD CONTROLLER STUFF
+@dataclass
 class KeyListenerCommand:
     """ A command obj containing function to call based on key press. """
-    keys_to_press : None
+    name : str
+    on_press_vs_on_release : bool
+    key_combination : tuple
+    callback_func = lambda: None
 
-    def __init__(self, key_presses:)
+    def __call__(self):
+        """ Call the callback function """
+        self.callback_func()
+
 
 class MyKeyboardListener(pynput.keyboard.Controller):
     """ 
@@ -22,7 +30,7 @@ class MyKeyboardListener(pynput.keyboard.Controller):
 
     keyboard_keys_holddown = set()
 
-    keyboard_commands = list()
+    keylistener_commands = list()
 
     def __init__(self, *args, **kwargs):
         """ NB. SIngleton """
@@ -45,21 +53,43 @@ class MyKeyboardListener(pynput.keyboard.Controller):
             cls.stop()
 
     @classmethod
-    def add_command(cls, kl_cmd:KeyListenerCommand)->None:
-        """ 
-        Add a command to keyboard listener to look-out for. 
-        TODO: Add any other command filters.
-            example: make sure the type input doesn't exist already.
-        """
-        if isinstance(kl_cmd, KeyListenerCommand):
-            cls.keyboard_commands.append(kl_cmd)
+    def __check_command_registered_already(cls, kl_command:KeyListenerCommand)->bool:
+        """ Check to see if a key listener command
+         exists already by name and by key combinations in a list of commands """
+        command_name_exists = any((kl_command.name==cmd.name for cmd in cls.keylistener_commands))
+        key_combination_exists = False
+        kl_command_combinations = set(kl_command.key_combination)
+        for kl_cmd in cls.keylistener_commands:
+            if set(kl_cmd.key_combination) in 
+        return (command_name_exists or key_combination_exists)
 
     @classmethod
-    def remove_command(cls, key:tuple)->None:
+    def register_command(cls, kl_cmd:KeyListenerCommand)->None:
         """ 
-        Remove a command 
-        TODO: Do a check based on key presses?
+        Add a command to keyboard listener to look-out for. 
+        Add any other command filters.
         """
+        if not isinstance(kl_cmd, KeyListenerCommand):
+            raise TypeError("This is not a KeyListenerCommand.{}".format(kl_cmd))
+        elif cls.__check_command_registered_already(kl_cmd):
+            raise ValueError("KeyListenerCommand already exists.")
+        else:
+            cls.keylistener_commands.append(kl_cmd)
+
+    @classmethod
+    def unregister_command(cls, key:str, quiet=False)->KeyListenerCommand:
+        """ 
+        Remove a KeyboardListenerCommand 
+        """
+        if isinstance(key, str):
+            for kl_cmd in cls.keylistener_commands:
+                if kl_cmd.name == key:
+                    return cls.keylistener_commands.pop()
+            else:
+                if not quiet:
+                    raise ValueError("Key '{}' doesn't exists.".format(key))
+        else:
+            TypeError("'{}' is not a str.".format(key))
 
     def call_func_mapped_to_key(self, def_callback=lambda:None)->None:
         """ 
@@ -133,8 +163,10 @@ def type_type_template_file(type_template_path=config.TYPE_TEMPLATE_PATH)->None:
 
 
 # Default KeyboardListener Commands
-MyKeyboardListener.add_command()
-KeyListenerCommand
+MyKeyboardListener.register_command(KeyListenerCommand(name="hello_world", on_press_vs_on_release=False), callback_func=lambda:print("Hello World"))
+MyKeyboardListener.register_command(KeyListenerCommand(name="made_it", on_press_vs_on_release=False))
+MyKeyboardListener.register_command(KeyListenerCommand(name="hello_world", on_press_vs_on_release=False))
+MyKeyboardListener.register_command(KeyListenerCommand(name="hello_world", on_press_vs_on_release=False))
 
     keyboard_callback_functions = {
         # (Key.shift, pynput.keyboard.KeyCode(char='l'), ): lambda: print("We made it."),
